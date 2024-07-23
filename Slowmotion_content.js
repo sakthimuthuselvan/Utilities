5448537893c2d729cf3aaa7fc694ad4644b67822
Sakthi M <108061211+sakthimuthuselvan@users.noreply.github.com>
2024-07-23 13:09:26 +0530

    Create Slowmotion_content.js

---
+++ Slowmotion_content.js
@@ -0,0 +1,7 @@
+// import from material ui
+
+                      <Collapse in={viewMore} timeout={600} unmountOnExit>
+                        <div className="card-box">
+                          {this.cardBoxFunBuild()}
+                        </div>
+                      </Collapse>
