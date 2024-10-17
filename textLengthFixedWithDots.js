888e25d81bb34b583966d7211fab3e322aa06c93
Sakthi M <108061211+sakthimuthuselvan@users.noreply.github.com>
2024-10-17 15:11:41 +0530

    Update textLengthFixedWithDots.js

--- textLengthFixedWithDots.js
+++ textLengthFixedWithDots.js
@@ -1,6 +1,9 @@
-
   textLengthFixed = (text, size) => {
-    let letters = text.split("").slice(0, size).join("")
-    letters = letters + "..."
-    return letters;
+    if(text.split("").length > size){
+      let letters = text.split("").slice(0, size).join("")
+      letters = letters + "..."
+      return letters;
+    }else{
+      return text
+    }
   }
